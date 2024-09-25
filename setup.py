4be797bdbc03704f0ad9d6b71df4ed6af4acbe6e
carvas91 <carvas91@hotmail.com>
2024-09-24 20:55:20 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str)-> List[str]:
+    '''
+    This function returns the list of requirements'''
+    requirements = []
+
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+        
+    return requirements
+
+
+
+
+setup(
+    name="MLOps project",
+    author='Carlos Vasconez',
+    version='0.0.1',
+    author_email='carvas91@hotmail.com',
+    packages=find_packages(), 
+    install_requires = get_requirements('requirements.txt')
+
+)
