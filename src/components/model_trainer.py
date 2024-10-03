bcd8d532a78d1c08306ead95416b62c80070c0c3
carvas91 <carvas91@hotmail.com>
2024-10-03 10:46:15 -0400

    commit

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,4 +24,28 @@ class ModelTrainerConfig:
     trained_model_file_path = os.path.join('artifacts', 'model.pkl')
 
 
-class Model_trainer
+class Model_trainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Split training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1])
+            
+            models = {
+                "LinearRegression":LinearRegression(),
+                "K Neighbors Regressor": KNeighborsRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "XGBoost": XGBRegressor(),
+                "CatBoost": CatBoostRegressor(),
+                "AdaBoost": AdaBoostRegressor()}
+            
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test = X_test, y_test= y_test,  models = models)
+        except:
+            pass
