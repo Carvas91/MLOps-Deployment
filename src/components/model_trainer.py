c388882cbfbcee81b41fefa4260a7311b8275490
carvas91 <carvas91@hotmail.com>
2024-10-11 11:09:31 -0400

    commit

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,7 +38,7 @@ class Model_trainer:
                 test_array[:,-1])
             
             models = {
-                "LinearRegression":LinearRegression(),
+                "Linear Regression":LinearRegression(),
                 "K Neighbors Regressor": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Random Forest": RandomForestRegressor(),
@@ -58,7 +58,7 @@ class Model_trainer:
                     # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
-                "Gradient Boosting":{
+                "XGBoost":{
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
@@ -71,12 +71,12 @@ class Model_trainer:
                     'learning_rate':[.1,.01,.05,.001],
                     'n_estimators': [8,16,32,64,128,256]
                 },
-                "CatBoosting Regressor":{
+                "CatBoost":{
                     'depth': [6,8,10],
                     'learning_rate': [0.01, 0.05, 0.1],
                     'iterations': [30, 50, 100]
                 },
-                "AdaBoost Regressor":{
+                "AdaBoost":{
                     'learning_rate':[.1,.01,0.5,.001],
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
